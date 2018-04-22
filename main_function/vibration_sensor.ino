56ea7e454fe41db5c01110b08f702b9f9670f3d0
tt_umv <60070029@it.kmitl.ac.th>
2018-04-22 23:50:24 +0700

    add ID selector

--- main_function/vibration_sensor.ino
+++ main_function/vibration_sensor.ino
@@ -3,8 +3,30 @@ int TP_init(){
   return pulseIn(vib_pin, HIGH);
 }
 
+int selector(){
+  int count = 0, id = 0;
+  while (count == 5){
+    if(id > 5){
+      id = 5;
+      break;
+    }
+    int messure = TP_init();
+    if (messure == 0){
+      count++;
+      continue;
+    }
+    else{
+      id++;
+      count = 0;
+    }
+  }
+
+  return id;
+}
+
 void reAdy_knock(string password[]){
   int check = 0, afk = 0;
+  int id_use = selector();
   while(bluetooth avaliable){
     delay(100);
     int messure = TP_init();
